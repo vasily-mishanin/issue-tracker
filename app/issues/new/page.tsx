1585e8ff3f1e87b9be928a6cf12dd0360d9334fb
vasily <vasilymishanin@gmail.com>
2024-01-14 16:24:53 +0600

    implement clientside validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,31 @@
 'use client';
-import { Button, TextArea, TextField, Callout } from '@radix-ui/themes';
+import { Button, TextArea, TextField, Callout, Text } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
+type IssueForm = z.infer<typeof createIssueSchema>;
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 function NewIssuePage() {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState('');
 
   return (
@@ -39,6 +50,11 @@ function NewIssuePage() {
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors.title && (
+          <Text as='p' color='red'>
+            {errors.title.message}
+          </Text>
+        )}
         {/* <TextArea placeholder='Description' /> */}
         {/* To grab input from third party component */}
         <Controller
@@ -48,6 +64,11 @@ function NewIssuePage() {
             <SimpleMDE placeholder='Description' {...field} /> // field is a one field from 'register(name)' object
           )}
         />
+        {errors.description && (
+          <Text as='p' color='red'>
+            {errors.description.message}
+          </Text>
+        )}
 
         <Button>Submit new issue</Button>
       </form>
