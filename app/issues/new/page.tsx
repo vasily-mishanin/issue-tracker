38a36d77d9b363d7514c6b7fb10e24a4968644f6
vasily <vasilymishanin@gmail.com>
2024-01-14 15:00:50 +0600

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 'use client';
-import { Button, TextArea, TextField } from '@radix-ui/themes';
+import { Button, TextArea, TextField, Callout } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 interface IssueForm {
   title: string;
@@ -14,29 +15,43 @@ interface IssueForm {
 function NewIssuePage() {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState('');
 
   return (
-    <form
-      className='max-w-xl space-y-3'
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder='Title' {...register('title')} />
-      </TextField.Root>
-      {/* <TextArea placeholder='Description' /> */}
-      <Controller
-        name='description'
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder='Description' {...field} /> // field is a one field from 'register(name)' object
-        )}
-      />
+    <div className='max-w-xl'>
+      {error && (
+        <Callout.Root color='red' className='mb-2'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className='space-y-3'
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+          } catch (error) {
+            console.log(error);
+            setError('An unexpected error occured');
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder='Title' {...register('title')} />
+        </TextField.Root>
+        {/* <TextArea placeholder='Description' /> */}
+        {/* To grab input from third party component */}
+        <Controller
+          name='description'
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder='Description' {...field} /> // field is a one field from 'register(name)' object
+          )}
+        />
 
-      <Button>Submit new issue</Button>
-    </form>
+        <Button>Submit new issue</Button>
+      </form>
+    </div>
   );
 }
 export default NewIssuePage;
