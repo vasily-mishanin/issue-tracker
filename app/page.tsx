c34b7b8cff04ca30e2d047dbe678c1931d6f36ce
Vasily Mishanin <vasilymishanin@gmail.com>
2024-06-16 16:41:46 +0600

    build issues summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
+import { Suspense } from 'react';
+import IssuesSummary from './IssuesSummary';
 import LatestIssues from './LatestIssues';
+import { Spinner } from './components';
 
-export default function Home() {
+export default async function Home() {
+  const openIssues =
+    (await prisma?.issue.count({
+      where: { status: 'OPEN' },
+    })) ?? 0;
+
+  const inProgressIssues =
+    (await prisma?.issue.count({
+      where: { status: 'IN_PROGRESS' },
+    })) ?? 0;
+
+  const closedIssues =
+    (await prisma?.issue.count({
+      where: { status: 'CLOSED' },
+    })) ?? 0;
+
   return (
     <div>
+      <Suspense fallback={<Spinner />}>
+        <IssuesSummary
+          open={openIssues}
+          closed={closedIssues}
+          inProgress={inProgressIssues}
+        />
+      </Suspense>
       <LatestIssues />
     </div>
   );
